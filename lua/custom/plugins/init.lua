88259f0837a1d641371392c6a2733657a53a265b
Dan Aloni <dan@kernelim.com>
2025-10-13 09:40:50 +0300

    Editor config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,10 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- EditorConfig support for automatic settings from .editorconfig files
+  {
+    'editorconfig/editorconfig-vim',
+    lazy = false, -- Load immediately to ensure settings are applied on startup
+  },
+}
