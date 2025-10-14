f7124f96ec93358667c2f9ec7dfba6a3ff57b99b
Dan Aloni <dan@kernelim.com>
2025-10-14 13:22:17 +0300

    Load Github's theme

--- init.lua
+++ init.lua
@@ -1718,6 +1718,21 @@ require('lazy').setup({
     end,
   },
 
+  -- Github theme
+  {
+    'projekt0n/github-nvim-theme',
+    name = 'github-theme',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('github-theme').setup {
+        -- ...
+      }
+
+      vim.cmd 'colorscheme github_dark'
+    end,
+  },
+
   { -- FZF fuzzy finder
     'junegunn/fzf.vim',
     dependencies = { 'junegunn/fzf' },
